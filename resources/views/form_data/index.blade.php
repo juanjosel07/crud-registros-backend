be87150483ddf6b5cf10cd1f12f7666a8ee65a2a
juanjosel07 <jujoleca21@gmail.com>
2025-05-22 16:58:13 -0500

    fix: se arreglo boton eliminar

--- resources/views/form_data/index.blade.php
+++ resources/views/form_data/index.blade.php
@@ -27,13 +27,6 @@
                                     <a class="btn btn-edit" href="{{ route('formdata.edit', $registro->id) }}"> 
                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
                                     </a>
-                                    {{-- <form action="{{ route('formdata.destroy', $registro->id) }}" method="POST" style="display:inline;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-remove">
-                                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
-                                        </button>
-                                    </form> --}}
 
                                     <button class="btn btn-remove"
                                         onclick="deleteRegister({{ $registro->id }})">
@@ -49,15 +42,6 @@
                                     </form>
                                 </div>
                             </td>
-                            {{-- <td class="border-br-lg">
-                                <a href="{{ route('formdata.edit', $registro->id) }}" class="btn btn-sm btn-warning">Editar</a>
-                            
-                                <form action="{{ route('formdata.destroy', $registro->id) }}" method="POST" style="display:inline;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-sm btn-danger" onclick="return confirm('¿Estás seguro?')">Eliminar</button>
-                                </form>
-                            </td> --}}
                         </tr>
                     @endforeach
                 </tbody>
